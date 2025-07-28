d0776630c3a5b0317b5943b84968a8d3e51d6452
fitrinrchmwt <fitriana11122003@gmail.com>
2025-07-29 03:44:06 +0700

    update view produk

--- resources/views/produk/produk.blade.php
+++ resources/views/produk/produk.blade.php
@@ -37,10 +37,36 @@
                 </div>
 
                 <!-- End Modal -->
+                
+                
+                <form id="formFilterProduk" class="row mb-3 formFilterProduk">
+                    <div class="col-md-3">
+                        <input type="text" class="form-control" id="filter_nama_produk" placeholder="Masukkan Nama Produk">
+                    </div>
+                    <div class="col-md-2">
+                        <select id="filter_satuan" name="satuan" class="form-control">
+                                    <option value="">-- Pilih Satuan --</option>
+                                    <option value="pcs">Pcs</option>
+                                    <option value="pack">Pack</option>
+                                </select>
+                    </div>
+                    <div class="col-md-2">
+                        <input type="number" class="form-control" id="filter_stok_min" placeholder="Stok Minimum" min="0">
+                    </div>
+                    <div class="col-md-2">
+                        <input type="number" class="form-control" id="filter_stok_max" placeholder="Stok Maksimum" min="0">
+                    </div>
+                    <div class="col-md-2 btnFilter" id="btnFilter">
+                        <button type="button" class="btn btn-damava" >
+                            <i class="fas fa-search"></i> Cari
+                        </button>
+                    </div>
+                </form>
+                
 
                 <!-- Table -->
                 <div class="scroll-table-container">
-                    <table class="table table-bordered table-striped text-center" id="tabel-produk">
+                    <table class="table table-bordered table-striped text-center" id="dataTable">
                         <thead>
                             <tr>
                                 <th>No.</th>
@@ -237,3 +263,193 @@
 
 
 @endsection
+
+
+
+@push('scripts')
+<script src="https://cdn.jsdelivr.net/npm/moment@2.29.4/moment.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<script>
+   $('#btnFilter button').click(function () {
+    var nama_produk = $('#filter_nama_produk').val();
+    var satuan = $('#filter_satuan').val();
+    var stok_min = $('#filter_stok_min').val();
+    var stok_max = $('#filter_stok_max').val();
+    var tanggal = $('#filter_tanggal').val();
+    var updated_at = $('#filter_updated_at').val();
+
+    $.ajax({
+        url: '{{ secure_url("/produk/filter") }}',
+        type: 'GET',
+        data: {
+            nama_produk: nama_produk,
+            satuan: satuan,
+            stok_min: stok_min,
+            stok_max: stok_max,
+            tanggal: tanggal,
+            updated_at: updated_at
+        },
+        success: function (response) {
+            let rows = '';
+            let total = 0;
+            let no = 1;
+
+            if (response.status === 'success') {
+                if (response.data.length > 0) {
+                    $.each(response.data, function (index, item) {
+                        total += parseInt(item.stok_produk);
+
+                        rows += `<tr>
+                            <td>${no++}</td>
+                            <td>${item.id_produk}</td>
+                            <td>${item.nama_produk ?? '-'}</td>
+                            <td>${item.satuan}</td>
+                            <td>${item.stok_produk ?? '-'}</td>
+                            <td width="28%">
+                                <a href="#" class="btn btn-info btn-sm shadow-sm" data-bs-toggle="modal"
+                                                    data-bs-target="#detailProdukModal${item.id_produk}">
+                                                    <i class="fas fa-file-alt fa-sm text-white-50"></i> Detail
+                                                </a>
+                                |
+                                <button class="btn btn-warning btn-sm shadow-sm edit-product-btn"
+                                    data-id_produk="${item.id_produk}"
+                                    data-nama_produk="${item.nama_produk}" 
+                                    data-satuan="${item.satuan}"
+                                    data-stok_produk="${item.stok_produk}">
+                                    <i class="fas fa-edit fa-sm text-white-50"></i> Edit
+                                </button> |
+                                <button class="btn btn-danger btn-sm shadow-sm delete-product-btn" data-id="${item.id_produk}">
+                                    <i class="fas fa-trash-alt fa-sm text-white-50"></i> Hapus
+                                </button>
+
+                                <!-- Modal Detail -->
+                                                <div class="modal fade" id="detailProdukModal${ item.id_produk }"
+                                                    tabindex="-1" aria-labelledby="modalLabel${ item.id_produk }"
+                                                    aria-hidden="true">
+                                                    <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="modalLabel${ item.id_produk }">Detail
+                                                                    Produk</h5>
+                                                                <button type="button" class="close" data-bs-dismiss="modal"
+                                                                    aria-label="Tutup">
+                                                                    <span aria-hidden="true">&times;</span>
+                                                                </button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <table class="table table-bordered">
+                                                                    <tr>
+                                                                        <td>ID Produk</td>
+                                                                        <td>${ item.id_produk }</td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>Nama Produk</td>
+                                                                        <td>${ item.nama_produk }</td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>Satuan</td>
+                                                                        <td>${ item.satuan }</td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>Jumlah</td>
+                                                                        <td>${ item.stok_produk }</td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>Tanggal Pencatatan</td>
+                                                                        <td>${ item.created_at ? moment.utc(item.created_at).format('DD/MM/YYYY') : '-' }
+                                                                        </td>
+                                                                    </tr>
+
+                                                                    <tr>
+                                                                        <td>Terakhir Diubah</td>
+                                                                        <td>${ item.updated_at ? moment.utc(item.updated_at).format('DD/MM/YYYY') : '-' }
+                                                                        </td>
+                                                                    
+                                                                    <tr>
+                                                                        <td>ID User</td>
+                                                                        <td>${ item.id_user ?? '-' }</td>
+                                                                    </tr>
+                                                                </table>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary"
+                                                                    data-bs-dismiss="modal">Tutup</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- Modal Detail End -->
+                            </td>
+                        </tr>`;
+                    });
+
+                    rows += `<tr>
+                        <td colspan="4"><strong>Total</strong></td>
+                        <td><strong>${total}</strong></td>
+                        <td></td>
+                    </tr>`;
+                } else {
+                    rows = `<tr><td colspan="6" class="text-center">Tidak ada data ditemukan.</td></tr>`;
+                }
+
+                $('#dataTable tbody').html(rows);
+            }
+        },
+        error: function () {
+            alert('Gagal mengambil data.');
+        }
+    });
+});
+</script>
+
+<script>
+    // Edit
+$(document).on('click', '.edit-product-btn', function() {
+    $('#edit_id_produk').val($(this).data('id_produk'));
+    $('#edit_nama_produk').val($(this).data('nama_produk'));
+    $('#edit_satuan').val($(this).data('satuan'));
+    $('#edit_stok_produk').val($(this).data('stok_produk'));
+    $('#editProdukModal').modal('show');
+});
+
+// Detail
+$(document).on('click', '.detail-product-btn', function() {
+    var id = $(this).data('id');
+    // AJAX untuk ambil detail produk, lalu tampilkan di modal
+    // Atau tampilkan modal yang sudah ada jika data sudah tersedia
+});
+
+// Hapus
+$(document).on('click', '.delete-product-btn', function() {
+    var id = $(this).data('id');
+    var csrf = $('meta[name="csrf-token"]').attr('content');
+    var baseUrl = "{{ secure_url('hapusproduk') }}";
+    var url = `${baseUrl}/${id}`;
+    if (confirm('Yakin ingin menghapus produk ini?')) {
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: {
+                _token: csrf,
+                _method: 'DELETE'
+            },
+            success: function(res) {
+                // Refresh tabel setelah hapus
+                $('#btnFilter button').click();
+                // Tampilkan notifikasi sukses dengan SweetAlert
+                Swal.fire({
+                    title: 'Sukses!',
+                    text: 'Produk berhasil dihapus.',
+                    icon: 'success',
+                    confirmButtonText: 'OK'
+                });
+            },
+            error: function() {
+                alert('Produk gagal dihapus.');
+            }
+        });
+    }
+});
+</script>
+@endpush
